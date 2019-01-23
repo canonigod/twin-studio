91c71085af853b127492229c4311bd4f04e014a9
David Canonigo <dcanonigo17@gmail.com>
2019-01-23 23:58:43 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let nav = document.getElementById("nav");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    nav.classList.add("fixed-top");
+  } else {
+    nav.classList.remove("fixed-top");
+  }
+}
