e128bc883f3bde98637f98ea9a1e9f969f62979d
David Canonigo <dcanonigo17@gmail.com>
2019-01-26 22:22:48 +0000

    responsive to iPad and iPad pro

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-let nav = document.getElementById("nav");
+// let nav = document.getElementById("nav");
 
-window.onscroll = function() {
-  scrollFunction();
-};
+// window.onscroll = function() {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    nav.classList.add("fixed-top");
-  } else {
-    nav.classList.remove("fixed-top");
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     nav.classList.add("fixed-top");
+//   } else {
+//     nav.classList.remove("fixed-top");
+//   }
+// }
 
-$(function() {
-  // $(document).ready shorthand
-  $(".monster").fadeIn("slow");
-});
+// $(function() {
+//   // $(document).ready shorthand
+//   $(".monster").fadeIn("slow");
+// });
